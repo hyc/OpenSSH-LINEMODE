8efc0c151a0f57612e1aa0abb5b7d536db4cf76a
damien <damien>
1999-10-27 03:42:43 +0000

    Initial revision

---
+++ compat.c
@@ -0,0 +1,10 @@
+#include "includes.h"
+RCSID("$Id$");
+
+#include "ssh.h"
+
+int compat13=0;
+void enable_compat13(void){
+	log("Enabling compatibility mode for protocol 1.3");
+	compat13=1;
+}
---
+++ compat.h
@@ -0,0 +1,7 @@
+/* RCSID("$Id$"); */
+
+#ifndef COMPAT_H
+#define COMPAT_H
+void enable_compat13(void);
+extern int compat13;
+#endif
