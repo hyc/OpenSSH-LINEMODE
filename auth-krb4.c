f2effd933f668bcb568c5c59f02e08b9e5df4b90
damien <damien>
1999-11-08 04:49:41 +0000

    Merged latest OpenBSD CVS changes:  - remove unused argument. ok dugsong

--- auth-krb4.c
+++ auth-krb4.c
@@ -171,11 +171,12 @@ auth_kerberos_tgt_failure:
   return 0;
 }
 
-int auth_afs_token(char *server_user, uid_t uid, const char *string)
+int auth_afs_token(struct passwd *pw, const char *token_string)
 {
   CREDENTIALS creds;
+  uid_t uid = pw->pw_uid;
 
-  if (!radix_to_creds(string, &creds)) {
+  if (!radix_to_creds(token_string, &creds)) {
     log("Protocol error decoding AFS token");
     packet_send_debug("Protocol error decoding AFS token");
     packet_start(SSH_SMSG_FAILURE);
