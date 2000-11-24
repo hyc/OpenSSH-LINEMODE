301c7da50fce3df3b52596bdf238b15cd580f5c9
djm <djm>
2000-11-24 23:09:32 +0000

    give up privs when reading seed file

--- entropy.c
+++ entropy.c
@@ -798,7 +798,10 @@ seed_rng(void)
 
 void init_rng(void) 
 {
+	int original_euid;
+	
 	original_uid = getuid();
+	original_euid = geteuid();
 
 	/* Read in collection commands */
 	if (!prng_read_commands(SSH_PRNG_COMMAND_FILE))
@@ -806,7 +809,16 @@ void init_rng(void)
 
 	/* Set ourselves up to save a seed upon exit */
 	prng_seed_saved = 0;		
+
+	/* Give up privs while reading seed file */
+	if ((original_uid != original_euid) && (seteuid(original_uid) == -1))
+		fatal("Couldn't give up privileges");
+	
 	prng_read_seedfile();
+
+	if ((original_uid != original_euid) && (seteuid(original_euid) == -1))
+		fatal("Couldn't restore privileges");
+
 	fatal_add_cleanup(prng_seed_cleanup, NULL);
 	atexit(prng_write_seedfile);
 
