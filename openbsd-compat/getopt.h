9ccb830f176cc97627ee5f5297444021cb7cfee2
djm <djm>
2001-09-18 05:05:20 +0000

    - (djm) Avoid warning on BSDgetopt

--- openbsd-compat/getopt.h
+++ openbsd-compat/getopt.h
@@ -1,14 +1,14 @@
 /* $Id$ */
 
-#ifndef _GETOPT_H
-#define _GETOPT_H
+#ifndef _BSDGETOPT_H
+#define _BSDGETOPT_H
 
 #include "config.h"
 
-#ifndef HAVE_GETOPT_H
+#if !defined(HAVE_GETOPT) || !defined(HAVE_GETOPT_OPTRESET)
 
 int BSDgetopt(int argc, char * const *argv, const char *opts);
 
 #endif
 
-#endif /* _GETOPT_H */
+#endif /* _BSDGETOPT_H */
