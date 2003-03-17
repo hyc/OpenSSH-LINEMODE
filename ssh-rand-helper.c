31c3fa2123faa67e8b8ba2bce8885966224a7ba2
djm <djm>
2003-03-17 05:13:53 +0000

    - (djm) Fix return value checks for RAND_bytes. Report from    Steve G <linux_4ever@yahoo.com>

--- ssh-rand-helper.c
+++ ssh-rand-helper.c
@@ -562,7 +562,8 @@ prng_write_seedfile(void)
 
 	debug("writing PRNG seed to file %.100s", filename);
 
-	RAND_bytes(seed, sizeof(seed));
+	if (RAND_bytes(seed, sizeof(seed)) <= 0)
+		fatal("PRNG seed extration failed");
 
 	/* Don't care if the seed doesn't exist */
 	prng_check_seedfile(filename);
@@ -849,7 +850,8 @@ main(int argc, char **argv)
 	if (!RAND_status())
 		fatal("Not enough entropy in RNG");
 
-	RAND_bytes(buf, bytes);
+	if (RAND_bytes(buf, bytes) <= 0)
+		fatal("Couldn't extract entropy from PRNG");
 
 	if (output_hex) {
 		for(ret = 0; ret < bytes; ret++)
