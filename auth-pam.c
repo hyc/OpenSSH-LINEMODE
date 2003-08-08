df5e7657824dbfb40e0ea126a96d6a22a499f7d9
dtucker <dtucker>
2003-08-08 03:43:37 +0000

    - (dtucker) [auth-pam.c] Don't set PAM_TTY if tty is null.  ok djm@

--- auth-pam.c
+++ auth-pam.c
@@ -534,11 +534,13 @@ do_pam_session(const char *user, const char *tty)
 	if (sshpam_err != PAM_SUCCESS)
 		fatal("PAM: failed to set PAM_CONV: %s",
 		    pam_strerror(sshpam_handle, sshpam_err));
-	debug("PAM: setting PAM_TTY to \"%s\"", tty);
-	sshpam_err = pam_set_item(sshpam_handle, PAM_TTY, tty);
-	if (sshpam_err != PAM_SUCCESS)
-		fatal("PAM: failed to set PAM_TTY: %s",
-		    pam_strerror(sshpam_handle, sshpam_err));
+	if (tty != NULL) {
+		debug("PAM: setting PAM_TTY to \"%s\"", tty);
+		sshpam_err = pam_set_item(sshpam_handle, PAM_TTY, tty);
+		if (sshpam_err != PAM_SUCCESS)
+			fatal("PAM: failed to set PAM_TTY: %s",
+			    pam_strerror(sshpam_handle, sshpam_err));
+	}
 	sshpam_err = pam_open_session(sshpam_handle, 0);
 	if (sshpam_err != PAM_SUCCESS)
 		fatal("PAM: pam_open_session(): %s",
