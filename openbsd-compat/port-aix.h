6dcaf69830d3a4951d2ac895f876060eec4bdbc1
dtucker <dtucker>
2004-02-06 05:17:51 +0000

    - (dtucker) [openbsd-compat/port-aix.c openbsd-compat/port-aix.h] Restore    previous authdb setting after auth calls.  Fixes problems with setpcred    failing on accounts that use AFS or NIS password registries.

--- openbsd-compat/port-aix.h
+++ openbsd-compat/port-aix.h
@@ -51,6 +51,14 @@
 # include <sys/timers.h>
 #endif
 
+/*
+ * According to the setauthdb man page, AIX password registries must be 15
+ * chars or less plus terminating NUL.
+ */
+#ifdef HAVE_SETAUTHDB
+# define REGISTRY_SIZE	16
+#endif
+
 void aix_usrinfo(struct passwd *);
 
 #ifdef WITH_AIXAUTHENTICATE
@@ -60,5 +68,6 @@ void record_failed_login(const char *, const char *);
 
 int aix_authenticate(const char *, const char *, const char *);
 void aix_setauthdb(const char *);
+void aix_restoreauthdb(void);
 void aix_remove_embedded_newlines(char *);
 #endif /* _AIX */
