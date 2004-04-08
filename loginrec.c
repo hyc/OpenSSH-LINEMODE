3ef24a6de494273abd54b69bf62a9a96b8edcad1
dtucker <dtucker>
2004-04-08 00:57:05 +0000

    - (dtucker) [loginrec.c] Use UT_LINESIZE if available, prevents truncating    pty name on Linux 2.6.x systems.  Patch from jpe at eisenmenger.org.

--- loginrec.c
+++ loginrec.c
@@ -1354,7 +1354,11 @@ static int
 syslogin_perform_logout(struct logininfo *li)
 {
 # ifdef HAVE_LOGOUT
+# ifdef UT_LINESIZE
+	char line[UT_LINESIZE];
+# else
 	char line[8];
+# endif
 
 	(void)line_stripname(line, li->line, sizeof(line));
 
