3f1c2038f543cf9f9bd2522faf73b62b1a713282
dtucker <dtucker>
2005-01-20 11:07:29 +0000

    - (dtucker) [loginrec.h] Bug #952: Increase size of username field to 128    bytes to prevent errors from login_init_entry() when the username is    exactly 64 bytes(!) long.  From brhamon at cisco.com, ok djm@

--- loginrec.h
+++ loginrec.h
@@ -62,7 +62,7 @@ union login_netinfo {
 /* string lengths - set very long */
 #define LINFO_PROGSIZE 64
 #define LINFO_LINESIZE 64
-#define LINFO_NAMESIZE 64
+#define LINFO_NAMESIZE 128
 #define LINFO_HOSTSIZE 256
 
 struct logininfo {
