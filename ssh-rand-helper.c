21f7e3c3a5c280985708c2b9252df6f0908abf3e
dtucker <dtucker>
2005-02-16 02:32:30 +0000

    Document the path to seed_rng better

--- ssh-rand-helper.c
+++ ssh-rand-helper.c
@@ -891,8 +891,8 @@ main(int argc, char **argv)
 
 /*
  * We may attempt to re-seed during mkstemp if we are using the one in the
- * compat library (via mkstemp -> arc4random -> seed_rng) so we need
- * our own seed_rng().  We must also check that we have enough entropy.
+ * compat library (via mkstemp -> _gettemp -> arc4random -> seed_rng) so we
+ * need our own seed_rng().  We must also check that we have enough entropy.
  */
 void
 seed_rng(void)
