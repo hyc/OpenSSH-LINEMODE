3253aba404e8e00acb42d7ad14edeabf6dcd2ca1
djm <djm>
2005-11-24 08:58:19 +0000

    - (djm) [configure.ac openbsd-compat/Makefile.in openbsd-compat/bsd-asprintf.c    openbsd-compat/bsd-snprintf.c openbsd-compat/openbsd-compat.h] Add an    asprintf() implementation, after syncing our {v,}snprintf() implementation    with some extra fixes from Samba's version. With help and debugging from    dtucker and tim; ok dtucker@

--- openbsd-compat/openbsd-compat.h
+++ openbsd-compat/openbsd-compat.h
@@ -142,6 +142,10 @@ unsigned int arc4random(void);
 void arc4random_stir(void);
 #endif /* !HAVE_ARC4RANDOM */
 
+#ifndef HAVE_ASPRINTF
+int asprintf(char **, const char *, ...);
+#endif 
+
 #ifndef HAVE_OPENPTY
 int openpty(int *, int *, char *, struct termios *, struct winsize *);
 #endif /* HAVE_OPENPTY */
@@ -158,6 +162,10 @@ long long strtoll(const char *, char **, int);
 
 #ifndef HAVE_STRTONUM
 long long strtonum(const char *, long long, long long, const char **);
+#endif
+
+#ifndef HAVE_VASPRINTF
+int vasprintf(char **, const char *, va_list);
 #endif
 
 #ifndef HAVE_VSNPRINTF
