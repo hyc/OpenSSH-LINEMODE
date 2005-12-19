28bf5a4883af89ee7330a6cd91583d03ba5fc9d4
dtucker <dtucker>
2005-12-19 06:40:40 +0000

    - (dtucker) [cipher-aes.c cipher-ctr.c cipher.c configure.ac    openbsd-compat/openssl-compat.h] Check for and work around broken AES    ciphers >128bit on (some) Solaris 10 systems.  ok djm@

--- openbsd-compat/openssl-compat.h
+++ openbsd-compat/openssl-compat.h
@@ -24,7 +24,11 @@
 # define EVP_CIPHER_CTX_get_app_data(e)		((e)->app_data)
 #endif
 
-#if OPENSSL_VERSION_NUMBER < 0x00907000L
+#if (OPENSSL_VERSION_NUMBER < 0x00907000L) || defined(OPENSSL_LOBOTOMISED_AES)
+# define USE_BUILTIN_RIJNDAEL
+#endif
+
+#ifdef USE_BUILTIN_RIJNDAEL
 # define EVP_aes_128_cbc evp_rijndael
 # define EVP_aes_192_cbc evp_rijndael
 # define EVP_aes_256_cbc evp_rijndael
